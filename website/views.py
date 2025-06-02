310ab2b593f3ac42a288b7bcd07890574a703c99
JayPrinz <karanjajaymo44@gmail.com>
2025-06-02 20:13:38 +0300

    Add user registration feature: create register view, update URL routing, and add registration link in navbar template.

--- website/views.py
+++ website/views.py
@@ -41,3 +41,7 @@ def logout_user(request):
     logout(request)
     messages.success(request, "You have been logged out ...")
     return redirect('home')
+
+
+def register_user(request):
+    return render(request, 'register.html', {})
